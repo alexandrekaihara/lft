694ed265a58631a621e198905b0deb857225baef
Marcelo <marcelo.postiglione@gmail.com>
2023-10-07 13:31:43 -0300

    update gitignore

--- src/onos.py
+++ src/onos.py
@@ -5,7 +5,7 @@ import paramiko
 class ONOS(Controller):
     def instantiate(self, dockerImage="onosproject/onos", mapPorts = False) -> None:
         if mapPorts: dockerCommand = f"docker run -dit -p 8181:8181 -p 8101:8101 -p 5005:5005 -p 830:830 --privileged --name={self.getNodeName()} onosproject/onos"
-        else: dockerCommand = f"docker run -dit -p 8181 -p 8101 -p 5005 -p 830 --privileged --name={self.getNodeName()} onosproject/onos"
+        else: dockerCommand = f"docker run -dit --privileged --name={self.getNodeName()} onosproject/onos"
         return super().instantiate(dockerImage, dockerCommand)
     
     # Brief: Activate required ONOS apps automatically.
