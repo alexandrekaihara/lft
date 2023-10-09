ca59dec6ec0c07c107b8b12f6b717fcc2307b35d
Marcelo <marcelo.postiglione@gmail.com>
2023-10-09 20:01:07 -0300

    feat: Atomix and ONOS configuration files

--- src/atomix.py
+++ src/atomix.py
@@ -4,6 +4,6 @@ from node import Node
 
 class Atomix(Node):
     def instantiate(self, hostConfPath, containerConfPath='/etc/atomix/conf'):
-        command = f"docker run -it -v {hostConfPath}:{containerConfPath} atomix/atomix:3.0.7 --config {containerConfPath}/atomix.conf --ignore-resources"
+        command = f"docker run -it -v {hostConfPath}:{containerConfPath} atomix/atomix:3.1.12 --config {containerConfPath}/atomix.conf --ignore-resources"
         super().instantiate(dockerImage='atomix/atomix:3.1.12', dockerCommand=command)
         
