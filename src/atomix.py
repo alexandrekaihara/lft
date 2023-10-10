7151f24a9b1f9616b6e10bd80001ab4ebe51dd5d
Marcelo <marcelo.postiglione@gmail.com>
2023-10-09 22:40:40 -0300

    .

--- src/atomix.py
+++ src/atomix.py
@@ -4,6 +4,6 @@ from node import Node
 
 class Atomix(Node):
     def instantiate(self, hostConfPath, containerConfPath='/etc/atomix/conf'):
-        command = f"docker run -it -v {hostConfPath}:{containerConfPath} -name={self.getNodeName()} atomix/atomix:3.1.12 --config {containerConfPath}/atomix.conf --ignore-resources"
+        command = f"docker run -it -v {hostConfPath}:{containerConfPath} --name={self.getNodeName()} atomix/atomix:3.1.12 --config {containerConfPath}/atomix.conf --ignore-resources"
         super().instantiate(dockerImage='atomix/atomix:3.1.12', dockerCommand=command)
         
