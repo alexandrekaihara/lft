5211a1c9faf02f98301d70432f7fc04ccdf8c11a
alexandrekaihara <alexandreamk1@gmail.com>
2023-10-22 09:32:33 -0300

    Fix: Added set mtu size on interface

--- experiment/emu_phy_wired.py
+++ experiment/emu_phy_wired.py
@@ -8,5 +8,6 @@ class EmuPhyWired:
         self.h1.connectToInternet("10.0.0.1", 30, "h1host", "hosth1")
         self.h1.setIp("10.0.0.2", 24, "h1host")
         self.h1.setDefaultGateway("10.0.0.1", "h1host")
+        self.h1.setMtuSize("h1host", 9000)
     def tearDown(self):
         self.h1.delete()
