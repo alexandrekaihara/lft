8ea965c793c9a161192c32e34dc7a2182756709f
alexandrekaihara <alexandreamk1@gmail.com>
2023-11-08 20:13:01 -0300

    Fixed wireless experiment

--- experiment/emu_emu_wireless.py
+++ experiment/emu_emu_wireless.py
@@ -7,27 +7,26 @@ class EmuEmuWireless:
     def __init__(self):
         self.epc = EPC('epc')
         self.enb = EnB("enb")
-        self.ue = UE('ue1')
+        self.ue = UE('ue')
 
     def setup(self):
-        self.epc.instantiate(dockerImage="alexandremitsurukaihara/lft:perfsonar-toolkit-srsRAN")
-        self.enb.instantiate(dockerImage="alexandremitsurukaihara/lft:perfsonar-toolkit-srsRAN")
-        self.ue.instantiate(dockerImage="alexandremitsurukaihara/lft:perfsonar-toolkit-srsRAN")
+        self.epc.instantiate(dockerImage="alexandremitsurukaihara/lft:srsran-perfsonar-uhd")
+        self.enb.instantiate(dockerImage="alexandremitsurukaihara/lft:srsran-perfsonar-uhd")
+        self.ue.instantiate(dockerImage="alexandremitsurukaihara/lft:srsran-perfsonar-uhd")
 
         self.enb.connect(self.epc, "enbepc", "epcenb")
-        self.ue.connect(self.enb, "ue1enb", "enbue1")
+        self.ue.connect(self.enb, "ueenb", "enbue")
 
-        self.self.epc.setIp('10.0.0.1', 24, "epcenb")
+        self.epc.connectToInternet('10.0.0.3', 24, "epchost", "hostepc")
+
+        self.epc.setIp('10.0.0.1', 24, "epcenb")
         self.enb.setIp('10.0.0.2', 24, "enbepc")
-        self.enb.setIp('11.0.0.1', 30, "enbue1")
-        self.enb.setIp('11.0.0.5', 30, "enbue2")
-        self.ue.setIp('11.0.0.2', 29, "ue1enb")
+        self.enb.setIp('11.0.0.1', 30, "enbue")
+        self.ue.setIp('11.0.0.2', 29, "ueenb")
 
-        self.enb.connectToInternet('10.0.0.6', 24, "epchost", "hostepc")
-
-        self.epc.setDefaultGateway('10.0.0.6', "epchost")
-        self.enb.setDefaultGateway('10.0.0.6', "enbepc")
-        self.ue.setDefaultGateway('10.0.0.2', "ue1enb")
+        self.epc.setDefaultGateway('10.0.0.3', "epchost")
+        self.enb.setDefaultGateway('10.0.0.3', "enbepc")
+        self.ue.setDefaultGateway('10.0.0.2', "ueenb")
 
         # Define EPC config
         self.epc.setEPCAddress("10.0.0.1")
@@ -36,17 +35,17 @@ class EmuEmuWireless:
         # Define ENB Config
         self.enb.setEPCAddress("10.0.0.1")
         self.enb.setEnBAddress("10.0.0.2")
-        self.enb.setMultiUEEnBAddr("11.0.0.1", 2101, '11.0.0.1', 2100)
-        self.enb.setMultiUEUE1Addr("11.0.0.2", 2001, "11.0.0.1", 2000)
+        self.enb.setSingleUEEnBAddr("11.0.0.1", 2101, '11.0.0.1', 2100)
+        self.enb.setSingleUEUEAddr("11.0.0.2", 2001, "11.0.0.1", 2000)
 
         # Define UE Config
         self.ue.setUEID("001010123456780")
 
         self.epc.start()
-        self.enb.starGnuRadioMultiUE()
+        self.enb.starGnuRadioSingleUE()
         self.enb.start("11.0.0.1", 2101, "11.0.0.1", 2100)
         time.sleep(5)  
-        self.ue.start("11.0.0.2", 2001, "11.0.0.1", 2000)
+        self.ue.start("--rf.device_name=zmq --rf.device_args=\"tx_port=tcp://11.0.0.2:2001,rx_port=tcp://11.0.0.1:2000,id=ue,base_srate=23.04e6\"")
 
     def tearDown(self):
         self.epc.delete()
