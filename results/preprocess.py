d1c9c048aaf3d425df297cece49e0080a373c6ca
alexandrekaihara <alexandreamk1@gmail.com>
2023-11-12 21:18:35 -0300

    Adding remove outliers

--- results/preprocess.py
+++ results/preprocess.py
@@ -1,7 +1,19 @@
 from json import load
+import numpy as np
+
 
 class Preprocess:
     def readJson(self, path: str):
         with open(path, "r") as f:
             json = load(f)
         return json
+    
+    def remove_outliers(data):
+        mean_value = np.mean(data)
+        std_dev = np.std(data)
+
+        threshold = 3 * std_dev
+
+        filtered_data = [x for x in data if (mean_value - threshold) < x < (mean_value + threshold)]
+
+        return filtered_data
