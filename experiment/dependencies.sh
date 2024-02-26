70b4e498ddfb195421fcad16e821b78c7c288099
alexandrekaihara <alexandreamk1@gmail.com>
2024-02-26 06:33:39 -0300

    feat: Install Containernet Wifi

--- experiment/dependencies.sh
+++ experiment/dependencies.sh
@@ -6,3 +6,15 @@ curl -o perfsonar-release.list http://downloads.perfsonar.net/debian/perfsonar-r
 curl http://downloads.perfsonar.net/debian/perfsonar-official.gpg.key | tac | tac | apt-key add -
 apt update
 apt install -y perfsonar-toolkit
+
+
+# Install Mininet-Wifi
+git clone git://github.com/intrig-unicamp/mininet-wifi
+cd mininet-wifi
+sudo util/install.sh -Wlnfv
+
+# Install Container Net
+sudo apt-get install -y ansible git aptitude
+git clone https://github.com/ramonfontes/containernet.git
+sudo util/install.sh -W
+mv containernet/containernet ../
