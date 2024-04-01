efaf475f13aec22571f774dc1ae8d35202efbda3
alexandrekaihara <alexandreamk1@gmail.com>
2024-04-01 08:58:56 -0300

    adding set mtu no emu phy wireles

--- experiment/emu_phy_wireless.py
+++ experiment/emu_phy_wireless.py
@@ -1,5 +1,6 @@
 from experiment.constants import *
 from lft.ue import UE
+from subprocess import run
 
 class EmuPhyWireless:
     def __init__(self):
@@ -10,15 +11,17 @@ class EmuPhyWireless:
         self.ue.connectToInternet(EMU_PHY_WIRELESS_UE_IP_ADDR, 29, "uehost", "hostue")
         self.ue.setIp('10.0.0.2', 29, "uehost")
         self.ue.setDefaultGateway(EMU_PHY_WIRELESS_UE_IP_ADDR, "uehost")
+        self.ue.setMtuSize(9000)
+        run("ifconfig hostue mtu 9000", shell=True)
 
         self.ue.setDeviceName("uhd")
         self.ue.setTxGain(str(25))
         self.ue.setRxGain(str(25))
         self.ue.setDeviceArgs(f"type=x300,addr={EMU_PHY_WIRELESS_USRP_IP_ADDR}")
 
-        self.ue.start()
-
         self.ue.setHost(EMU_PHY_WIRELESS_UE_IP_ADDR)
+        self.ue.start()
+        
 
     def tearDown(self):
         self.ue.delete()
