e98005f9790823f593a7b9d8c736f575c81afd22
alexandrekaihara <alexandreamk1@gmail.com>
2024-04-01 01:35:11 -0300

    add set mtu size

--- experiment/emu_phy_wired.py
+++ experiment/emu_phy_wired.py
@@ -1,5 +1,6 @@
 from lft.host import Host
 from experiment.constants import *
+from subprocess import subprocess
 
 
 class EmuPhyWired:
@@ -12,5 +13,7 @@ class EmuPhyWired:
         self.h1.setDefaultGateway("10.0.0.1", "h1host")
         self.h1.setHost('10.0.0.1')
         self.h1.setInterfaceTraffic("h1host", "1gbit", 0.571, 1600)
+        self.h1.setMtuSize('h1host', 9000)
+        subprocess.run("ip link set dev hosth1 mtu 9000", shell=True)
     def tearDown(self):
         self.h1.delete()
