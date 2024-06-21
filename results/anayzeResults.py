33fcbc1e20b7bfac8263294caa0672ba12b6b62e
alexandrekaihara <alexandreamk1@gmail.com>
2024-06-20 21:25:38 -0300

    Adding plot for LFT vs Mininet comparisons

--- results/anayzeResults.py
+++ results/anayzeResults.py
@@ -17,15 +17,19 @@ def confidenceInterval(data, confidence=0.95):
     return stats.norm.interval(alpha=confidence, loc=mean, scale=stderr) 
 
 def clearOutliers(data):
-    mean = np.mean(data)
+    npData = np.array(data)
+    zScores = stats.zscore(data)
+    return npData[[abs(z) < 3 for z in zScores]]
+
+    '''mean = np.mean(data)
     std = np.std(data)
     threshold = 3
     filteredData = []
     for i in data:
-        z = (i-mean)/std
+        z = abs((i-mean)/std)
         if z < threshold:
             filteredData.append(i)
-    return filteredData
+    return filteredData'''
 
 def getPaths(path):
     paths = glob(path)
@@ -85,7 +89,7 @@ def loadData(path, keyName):
     collectedMetricsList = extractMetricsFromJsons(jsons, keyName)
     print(collectedMetricsList)
     flatListValues = flattenList(collectedMetricsList)
-    return clearOutliers(flatListValues)
+    return clearOutliers(clearOutliers(clearOutliers(flatListValues)))
 
 
 def simplePlot(emuEmu, emuPhy, phyPhy, experimentLabel, mediumType, unitOfMeasure):
@@ -111,6 +115,19 @@ def plotConfidenceInterval(ciEmuEmu, ciEmuPhy, ciPhyPhy, experimentLabel, medium
 
 
 def plotBars(emuEmu, ciEmuEmu, emuPhy, ciEmuPhy, phyPhy, ciPhyPhy, experimentName, medium, unitOfMeasure):
+    if (len(emuEmu) == 0):
+        emuEmu = [0]
+    if (len(emuPhy) == 0):
+        emuPhy = [0]
+    if (len(phyPhy) == 0):
+        phyPhy = [0]
+    if (ciEmuEmu == None):
+        ciEmuEmu = (0,0)
+    if (ciEmuPhy == None):
+        ciEmuPhy = (0,0)
+    if (ciPhyPhy == None):
+        ciPhyPhy = (0,0)
+    
     emulated_mean = mean(emuEmu)
     hybrid_mean = mean(emuPhy)
     physical_mean = mean(phyPhy)
@@ -118,7 +135,7 @@ def plotBars(emuEmu, ciEmuEmu, emuPhy, ciEmuPhy, phyPhy, ciPhyPhy, experimentNam
     means = [emulated_mean, hybrid_mean, physical_mean]
     errs = [ciEmuEmu[1] - emulated_mean, ciEmuPhy[1] - hybrid_mean, ciPhyPhy[1] - physical_mean]
     
-    higher = max(max([emuEmu, emuPhy, phyPhy]))
+    higher = max([max(emuEmu), max(emuPhy), max(phyPhy)])
     
     columns = ["Emulated Only", "Hybrid", "Physical Only"]
     colors = ["red", "green", "blue"]
@@ -130,6 +147,24 @@ def plotBars(emuEmu, ciEmuEmu, emuPhy, ciEmuPhy, phyPhy, ciPhyPhy, experimentNam
     plt.xticks(range(len(columns)), columns)
     plt.ylabel(f"{experimentName} ({unitOfMeasure})")
     plt.title(f'{medium} Experiment {experimentName}')
+    plt.show()
+
+
+def minLen(listOfLists):
+    return min([len(l) for l in listOfLists])
+
+
+def plotToolComparison(lftValues, lftErr, mnValues, mnErr, title, unitOfMeasure):
+    barWidth = 0.25
+    r1 = np.arange(len(lftValues))
+    r2 = [x + barWidth for x in r1]
+    plt.title(title)
+    plt.bar(r1, lftValues, color='#7f6d5f', width=barWidth, edgecolor='white', label='LFT', yerr=lftErr, capsize=3)
+    plt.bar(r2, mnValues, color='#557f2d', width=barWidth, edgecolor='white', label='Mininet-WIFI (Containernet)', yerr=mnErr, capsize=3)
+    plt.xticks([r + barWidth for r in range(len(lftValues))], ['1', '4', '16', '64', '256'])
+    plt.xlabel('Medium', fontweight='bold')
+    plt.ylabel(f"{title} ({unitOfMeasure})")
+    plt.legend()
     plt.show()
 
 
@@ -166,6 +201,65 @@ wirelessEmuEmuLatencyData = loadData(RESULTS_PATH + EMU_EMU_WIRELESS_PREFIX + LA
 wirelessEmuPhyLatencyData = loadData(RESULTS_PATH + EMU_PHY_WIRELESS_PREFIX + LATENCY_JSON_FORMAT_WILDCARD, LATENCY)
 wirelessPhyPhyLatencyData = loadData(RESULTS_PATH + PHY_PHY_WIRELESS_PREFIX + LATENCY_JSON_FORMAT_WILDCARD, LATENCY)
 
+# Comparison
+## Deployment Time
+lftDeployTimeDf = pd.read_csv("results/data/deployLftTime.csv")
+mnDeployTimeDf = pd.read_csv("results/data/deployMnTime.csv")
+
+lftDeployTime1 = list(lftDeployTimeDf['1'])
+lftDeployTime4 = list(lftDeployTimeDf['4'])
+lftDeployTime16 = list(lftDeployTimeDf['16'])
+lftDeployTime64 = list(lftDeployTimeDf['64'])
+lftDeployTime248 = list(lftDeployTimeDf['248'])
+
+mnDeployTime1 = list(mnDeployTimeDf['1'])
+mnDeployTime4 = list(mnDeployTimeDf['4'])
+mnDeployTime16 = list(mnDeployTimeDf['16'])
+mnDeployTime64 = list(mnDeployTimeDf['64'])
+mnDeployTime248 = list(mnDeployTimeDf['248'])
+
+lftDeploymentValues = [mean(lftDeployTime1), mean(lftDeployTime4), mean(lftDeployTime16), mean(lftDeployTime64), mean(lftDeployTime248)]
+mininetDeploymentValues = [mean(mnDeployTime1), mean(mnDeployTime4), mean(mnDeployTime16), mean(mnDeployTime64), mean(mnDeployTime248)]
+
+## Undeployment Time
+lftUndeployTimeDf = pd.read_csv("results/data/undeployLftTime.csv")
+mnUndeployTimeDf = pd.read_csv("results/data/undeployMnTime.csv")
+
+lftUndeployTime1 = list(lftUndeployTimeDf['1'])
+lftUndeployTime4 = list(lftUndeployTimeDf['4'])
+lftUndeployTime16 = list(lftUndeployTimeDf['16'])
+lftUndeployTime64 = list(lftUndeployTimeDf['64'])
+lftUndeployTime248 = list(lftUndeployTimeDf['248'])
+
+mnUndeployTime1 = list(mnUndeployTimeDf['1'])
+mnUndeployTime4 = list(mnUndeployTimeDf['4'])
+mnUndeployTime16 = list(mnUndeployTimeDf['16'])
+mnUndeployTime64 = list(mnUndeployTimeDf['64'])
+mnUndeployTime248 = list(mnUndeployTimeDf['248'])
+
+lftUndeploymentValues = [mean(lftUndeployTime1), mean(lftUndeployTime4), mean(lftUndeployTime16), mean(lftUndeployTime64), mean(lftUndeployTime248)]
+mininetUndeploymentValues = [mean(mnUndeployTime1), mean(mnUndeployTime4), mean(mnUndeployTime16), mean(mnUndeployTime64), mean(mnUndeployTime248)]
+
+## Memory Consumption
+lftDeployMemDf = pd.read_csv("results/data/deployLftMem.csv")
+mnDeployMemDf = pd.read_csv("results/data/deployMnMem.csv")
+
+lftDeployMem1 = list(lftDeployMemDf['1'])
+lftDeployMem4 = list(lftDeployMemDf['4'])
+lftDeployMem16 = list(lftDeployMemDf['16'])
+lftDeployMem64 = list(lftDeployMemDf['64'])
+lftDeployMem248 = list(lftDeployMemDf['248'])
+
+mnDeployMem1 = list(mnDeployMemDf['1'])
+mnDeployMem4 = list(mnDeployMemDf['4'])
+mnDeployMem16 = list(mnDeployMemDf['16'])
+mnDeployMem64 = list(mnDeployMemDf['64'])
+mnDeployMem248 = list(mnDeployMemDf['248'])
+
+lftDeploymentMemValues = [mean(lftDeployMem1), mean(lftDeployMem4), mean(lftDeployMem16), mean(lftDeployMem64), mean(lftDeployMem248)]
+mininetDeploymentMemValues = [mean(mnDeployMem1), mean(mnDeployMem4), mean(mnDeployMem16), mean(mnDeployMem64), mean(mnDeployMem248)]
+
+
 ####################### Get Confidence Intervals #######################
 # Wired
 ## Throughput
@@ -199,16 +293,76 @@ ciWirelessEmuEmuLatencyData = confidenceInterval(wirelessEmuEmuLatencyData)
 ciWirelessEmuPhyLatencyData = confidenceInterval(wirelessEmuPhyLatencyData)
 ciWirelessPhyPhyLatencyData = confidenceInterval(wirelessPhyPhyLatencyData)
 
+# LFT vs Mininet-WIFI
+## LFT
+### Deployment Time
+errLftDeployTime1  = confidenceInterval(lftDeployTime1)[1] - mean(lftDeployTime1)
+errLftDeployTime4  = confidenceInterval(lftDeployTime4)[1] - mean(lftDeployTime4)
+errLftDeployTime16  = confidenceInterval(lftDeployTime16)[0] - mean(lftDeployTime16)
+errLftDeployTime64  = confidenceInterval(lftDeployTime64)[0] - mean(lftDeployTime64)
+errLftDeployTime256  = confidenceInterval(lftDeployTime248)[0] - mean(lftDeployTime248)
+
+errLftDeployTime = [errLftDeployTime1, errLftDeployTime4, errLftDeployTime16, errLftDeployTime64, errLftDeployTime256]
+
+### Undeployment Time
+errLftUndeployTime1  = confidenceInterval(lftUndeployTime1)[1] - mean(lftUndeployTime1)
+errLftUndeployTime4  = confidenceInterval(lftUndeployTime4)[1] - mean(lftUndeployTime4)
+errLftUndeployTime16  = confidenceInterval(lftUndeployTime16)[0] - mean(lftUndeployTime16)
+errLftUndeployTime64  = confidenceInterval(lftUndeployTime64)[0] - mean(lftUndeployTime64)
+errLftUndeployTime256  = confidenceInterval(lftUndeployTime248)[0] - mean(lftUndeployTime248)
+
+errLftUndeployTime = [errLftUndeployTime1, errLftUndeployTime4, errLftUndeployTime16, errLftUndeployTime64, errLftUndeployTime256]
+
+## Memory Consumption
+errLftDeployMem1  = confidenceInterval(lftDeployMem1)[1] - mean(lftDeployMem1)
+errLftDeployMem4  = confidenceInterval(lftDeployMem4)[1] - mean(lftDeployMem4)
+errLftDeployMem16  = confidenceInterval(lftDeployMem16)[0] - mean(lftDeployMem16)
+errLftDeployMem64  = confidenceInterval(lftDeployMem64)[0] - mean(lftDeployMem64)
+errLftDeployMem256  = confidenceInterval(lftDeployMem248)[0] - mean(lftDeployMem248)
+
+errLftDeployMem = [errLftDeployMem1, errLftDeployMem4, errLftDeployMem16, errLftDeployMem64, errLftDeployMem256]
+
+## Mininet
+### Deployment Time
+errMnDeployTime1  = confidenceInterval(mnDeployTime1)[1] - mean(mnDeployTime1)
+errMnDeployTime4  = confidenceInterval(mnDeployTime4)[1] - mean(mnDeployTime4)
+errMnDeployTime16  = confidenceInterval(mnDeployTime16)[0] - mean(mnDeployTime16)
+errMnDeployTime64  = confidenceInterval(mnDeployTime64)[0] - mean(mnDeployTime64)
+errMnDeployTime256  = confidenceInterval(mnDeployTime248)[0] - mean(mnDeployTime248)
+
+errMnDeployTime = [errMnDeployTime1, errMnDeployTime4, errMnDeployTime16, errMnDeployTime64, errMnDeployTime256]
+
+### Undeployment Time
+errMnUndeployTime1  = confidenceInterval(mnUndeployTime1)[1] - mean(mnUndeployTime1)
+errMnUndeployTime4  = confidenceInterval(mnUndeployTime4)[1] - mean(mnUndeployTime4)
+errMnUndeployTime16  = confidenceInterval(mnUndeployTime16)[0] - mean(mnUndeployTime16)
+errMnUndeployTime64  = confidenceInterval(mnUndeployTime64)[0] - mean(mnUndeployTime64)
+errMnUndeployTime256  = confidenceInterval(mnUndeployTime248)[0] - mean(mnUndeployTime248)
+
+errMnUndeployTime = [errMnUndeployTime1, errMnUndeployTime4, errMnUndeployTime16, errMnUndeployTime64, errMnUndeployTime256]
+
+## Memory Consumption
+errMnDeployMem1  = confidenceInterval(mnDeployMem1)[1] - mean(mnDeployMem1)
+errMnDeployMem4  = confidenceInterval(mnDeployMem4)[1] - mean(mnDeployMem4)
+errMnDeployMem16  = confidenceInterval(mnDeployMem16)[0] - mean(mnDeployMem16)
+errMnDeployMem64  = confidenceInterval(mnDeployMem64)[0] - mean(mnDeployMem64)
+errMnDeployMem256  = confidenceInterval(mnDeployMem248)[0] - mean(mnDeployMem248)
+
+errMnDeployMem = [errMnDeployMem1, errMnDeployMem4, errMnDeployMem16, errMnDeployMem64, errMnDeployMem256]
+
+
 
 ####################### Plots #######################
 # Wired
 ## Throughput
-simplePlot(wiredEmuEmuThroughputData, wiredEmuPhyThroughputData, wiredPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRED, THROUGHPUT_UNIT_OF_MEASURE)
+minimunLength = minLen([wiredEmuEmuThroughputData, wiredEmuPhyThroughputData, wiredPhyPhyThroughputData])
+simplePlot(wiredEmuEmuThroughputData[:minimunLength], wiredEmuPhyThroughputData[:minimunLength], wiredPhyPhyThroughputData[:minimunLength], THROUGHPUT_EXPERIMENT_NAME, WIRED, THROUGHPUT_UNIT_OF_MEASURE)
 #plotConfidenceInterval(ciWiredEmuEmuThroughputData, ciWiredEmuPhyThroughputData, ciWiredPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRED)
 plotBars(wiredEmuEmuThroughputData, ciWiredEmuEmuThroughputData, wiredEmuPhyThroughputData, ciWiredEmuPhyThroughputData, wiredPhyPhyThroughputData, ciWiredPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRED, THROUGHPUT_UNIT_OF_MEASURE)
 
 ## RTT
-simplePlot(wiredEmuEmuRttData, wiredEmuPhyRttData, wiredPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRED, RTT_UNIT_OF_MEASURE)
+minimunLength = minLen([wiredEmuEmuRttData, wiredEmuPhyRttData, wiredPhyPhyRttData])
+simplePlot(wiredEmuEmuRttData[:minimunLength], wiredEmuPhyRttData[:minimunLength], wiredPhyPhyRttData[:minimunLength], RTT_EXPERIMENT_NAME, WIRED, RTT_UNIT_OF_MEASURE)
 #plotConfidenceInterval(ciWiredEmuEmuRttData, ciWiredEmuPhyRttData, ciWiredPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRED)
 plotBars(wiredEmuEmuRttData, ciWiredEmuEmuRttData, wiredEmuPhyRttData, ciWiredEmuPhyRttData, wiredPhyPhyRttData, ciWiredPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRED, RTT_UNIT_OF_MEASURE)
 
@@ -220,48 +374,40 @@ plotBars(wiredEmuEmuLatencyData, ciWiredEmuEmuLatencyData, wiredEmuPhyLatencyDat
 
 # Wireless
 ## Throughput
-simplePlot(wirelessEmuEmuLatencyData, wirelessEmuPhyThroughputData, wirelessPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRELESS, THROUGHPUT_UNIT_OF_MEASURE)
+simplePlot(wirelessEmuEmuThroughputData, wirelessEmuPhyThroughputData, wirelessPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRELESS, THROUGHPUT_UNIT_OF_MEASURE)
 #plotConfidenceInterval(ciWirelessEmuEmuThroughputData, ciWirelessEmuPhyThroughputData, ciWirelessPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRELESS)
-plotBars(wirelessEmuEmuThroughputData, ciWirelessEmuEmuThroughputData, wirelessEmuPhyThroughputData, ciWirelessEmuPhyThroughputData, wirelessPhyPhyThroughputData, ciWirelessPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRELESS, LATENCY_UNIT_OF_MEASURE)
+plotBars(wirelessEmuEmuThroughputData, ciWirelessEmuEmuThroughputData, wirelessEmuPhyThroughputData, ciWirelessEmuPhyThroughputData, wirelessPhyPhyThroughputData, ciWirelessPhyPhyThroughputData, THROUGHPUT_EXPERIMENT_NAME, WIRELESS, THROUGHPUT_UNIT_OF_MEASURE)
 
 ## RTT
-simplePlot(wirelessEmuEmuRttData, wirelessEmuPhyRttData, wirelessPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRELESS, RTT_UNIT_OF_MEASURE)
+minimunLength = minLen([wirelessEmuEmuRttData, wirelessPhyPhyRttData])
+simplePlot(wirelessEmuEmuRttData[:minimunLength], wirelessEmuPhyRttData[:minimunLength], wirelessPhyPhyRttData[:minimunLength], RTT_EXPERIMENT_NAME, WIRELESS, RTT_UNIT_OF_MEASURE)
 #plotConfidenceInterval(ciWirelessEmuEmuRttData, ciWirelessEmuPhyRttData, ciWirelessPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRELESS)
 plotBars(wirelessEmuEmuRttData, ciWirelessEmuEmuRttData, wirelessEmuPhyRttData, ciWirelessEmuPhyRttData, wirelessPhyPhyRttData, ciWirelessPhyPhyRttData, RTT_EXPERIMENT_NAME, WIRELESS, RTT_UNIT_OF_MEASURE)
 
 ## Latency
-simplePlot(wirelessEmuEmuLatencyData, wirelessEmuPhyLatencyData, wirelessPhyPhyLatencyData, LATENCY_EXPERIMENT_NAME, WIRELESS, LATENCY_UNIT_OF_MEASURE)
+minimunLength = minLen([wirelessEmuEmuLatencyData, wirelessPhyPhyLatencyData])
+simplePlot(wirelessEmuEmuLatencyData[:minimunLength], wirelessEmuPhyLatencyData[:minimunLength], wirelessPhyPhyLatencyData[:minimunLength], LATENCY_EXPERIMENT_NAME, WIRELESS, LATENCY_UNIT_OF_MEASURE)
 #plotConfidenceInterval(ciWirelessEmuEmuLatencyData, ciWirelessEmuPhyLatencyData, ciWirelessPhyPhyLatencyData, LATENCY_EXPERIMENT_NAME, WIRELESS)
 plotBars(wirelessEmuEmuLatencyData, ciWirelessEmuEmuLatencyData, wirelessEmuPhyLatencyData, ciWirelessEmuPhyLatencyData, wirelessPhyPhyLatencyData, ciWirelessPhyPhyLatencyData, LATENCY_EXPERIMENT_NAME, WIRELESS, LATENCY_UNIT_OF_MEASURE)
 
 
+# LFT vs Mininet Time
+## Deployment Time
+plotToolComparison(lftDeploymentValues, errLftDeployTime, mininetDeploymentValues, errMnDeployTime, "Deployment Time", "s")
 
-# TESTING
-# set width of bars
-barWidth = 0.25
+# Undeployment Time
+plotToolComparison(lftUndeploymentValues, errLftUndeployTime, mininetUndeploymentValues, errMnUndeployTime, "Undeployment Time", "s")
 
-# set heights of bars
-emulated = [mean(wiredEmuEmuRttData), mean(wirelessEmuEmuRttData)]
-hybrid = [mean(wiredEmuPhyRttData), 41300]#mean(wirelessEmuPhyRttData)
-physical = [mean(wiredPhyPhyRttData), mean(wirelessPhyPhyRttData)]
+# DeploymentMem Time
+plotToolComparison(lftDeploymentMemValues, errLftDeployMem, mininetDeploymentMemValues, errMnDeployMem, "Deployment Memory Consumption", "bits")
 
-# Set position of bar on X axis
-r1 = np.arange(len(emulated))
-r2 = [x + barWidth for x in r1]
-r3 = [x + barWidth for x in r2]
 
-# Make the plot
-plt.bar(r1, emulated, color='#7f6d5f', width=barWidth, edgecolor='white', label='Fully Emulated')
-plt.bar(r2, hybrid, color='#557f2d', width=barWidth, edgecolor='white', label='Hybrid')
-plt.bar(r3, physical, color='#2d7f5e', width=barWidth, edgecolor='white', label='Fully Physical')
 
-# Add xticks on the middle of the group bars
-plt.xlabel('Medium', fontweight='bold')
-plt.xticks([r + barWidth for r in range(len(emulated))], ['Wired', 'Wireless'])
 
-# Create legend & Show graphic
-plt.legend()
-plt.show()
+
+
+
+
 
 
 
