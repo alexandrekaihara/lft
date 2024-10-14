02dafeb45e5efd6c6978476365d1c54cff40c3a4
alexandrekaihara <alexandreamk1@gmail.com>
2024-10-14 04:07:14 -0300

    Adding manifest

--- setup.py
+++ setup.py
@@ -17,18 +17,21 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
+import os
+import inspect
 
 class CustomInstall(install):
     def run(self):
-        subprocess.run("chmod +X dependencies.sh", shell=True)
-        subprocess.run("sudo ./dependencies.sh", shell=True)
+        currentPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        subprocess.run(f"chmod +X {currentPath}/dependencies.sh", shell=True)
+        subprocess.run(f"sudo .{currentPath}/dependencies.sh", shell=True)
         install.run(self)
     
 setup(
     name='profissa_lft',
-    version='1.0.7',
+    version='1.0.9',
     packages=find_packages(),
-    install_requires=['pandas'],
+    install_requires=['pandas', 'os', 'inspect'],
     author='Alexandre Mitsuru Kaihara',
     author_email='alexandreamk1@gmail.com',
     description='LFT is a framework designed to facilitate the creation of lightweight network topologies with ease. Using Docker containers, it is possible to add any container to the network to provide network services or even emulate network devices, such as switches, controllers (in Software Defined Networking). This project has integration with OpenvSwitch to emulate the network forwarding devices and srsRAN 4G to emulate wireless links for Fog and Edge application scenarios.',
@@ -43,6 +46,7 @@ setup(
     python_requires='>=3.9',
     cmdclass= {
         'install': CustomInstall
-    }
+    },
+    include_package_data=True
 )
 
