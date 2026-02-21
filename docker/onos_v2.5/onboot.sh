6d2949e5c59d10d67babd40cce8b6c2831411e7a
artdelpi <artdelpi@gmail.com>
2026-02-21 14:36:55 +0000

    fix: onboot.sh fix

--- docker/onos_v2.5/onboot.sh
+++ docker/onos_v2.5/onboot.sh
@@ -1,23 +1,9 @@
-#!/bin/bash
+./bin/onos-service server &
 
-#
-# Copyright (C) 2022 Alexandre Mitsuru Kaihara
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+while ! nc -z localhost 8101; do   
+  sleep 3
+done
 
-
-# Start container and keep alive
-tail -f /dev/null
-
+./bin/onos localhost app activate org.onosproject.proxyarp
+./bin/onos localhost app install! ./apps/link-quality.oar
+tail -f ./apache-karaf-*/data/log/karaf.log
